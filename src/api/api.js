1b8e11a10784f5ee149d42190fea7f4140e6a9ad
Jennifer Bach <jmbach1@gmail.com>
2018-06-13 09:35:35 -0700

    added comments and cleaned up files

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,30 @@
 'use strict';
 
 import express from 'express';
-const router = express.Router();
+import Assignment from '../models/assignment.js';
+import User from '../models/user.js';
 
-// Dynamic Models
-// This will use a model matching /:model/ in all routes that have a model parameter
-import modelFinder from '../middleware/models.js';
-router.param('model', modelFinder);
 
-// Each of our REST endpoints simply calls the model's appropriate CRUD Method (only give the students GET and POST for now)
-// In all cases, we just catch(next), which feeds any errors we get into the next() as a param
-// This fires off the error middleware automatically.  Otherwise, we send out a formatted JSON Response
-
-router.get('/api/v1/:model', (req,res,next) => {
-  req.model.find({})
-    .then( data => sendJSON(res,data) )
-    .catch( next );
+//Get a specific assignment by student ID and assignment ID
+router.get('api/v1/model/assignment/:studentid/:assignmentid', (req, res, next) => {
+  Assignment.find({})
+   .then (data => sendJSON(res, data))
+    .catch(next);
 });
 
-router.get('/api/v1/:model/:id', (req,res,next) => {
-  req.model.findOne({_id:req.params.id})
-    .then( data => sendJSON(res,data) )
-    .catch( next );
-});
-
-router.post('/api/v1/:model', (req,res,next) => {
-  let record = new req.model(req.body);
+//Post the student's notes for a specfic assignment
+router.post('api/v1/model/assignment/:studentid/:assignmentid', (req, res, next) => {
+  let note = new req.model(req.body);
   record.save()
-    .then( data => sendJSON(res,data) )
-    .catch( next );
-});
-
-router.put('/api/v1/:model/:id', (req, res, next) => {
-  req.model.findByIdAndUpdate(req.params.id, req.body)
-    .then(data => sendJSON(res, data))
+    .then (data => sendJSON(res, data))
     .catch(next);
 });
 
-router.delete('/api/v1/:model/:id', (req, res, next) => {
-  req.model.findByIdAndDelete(req.params.id)
-    .then(data => sendJSON(res, data))
+//Update the existing notes on an assignment
+router.put('api/v1/model/assignment/:studentid/:assignmentid', (req, res, next) => {
+  req.model.update(req.params.notes)
+    .then (data => sendJSON(res, data))
     .catch(next);
 });
-
-let sendJSON = (res,data) => {
-  res.statusCode = 200;
-  res.statusMessage = 'OK';
-  res.setHeader('Content-Type', 'application/json');
-  res.write( JSON.stringify(data) );
-  res.end();
-};
 
 export default router;
--- src/models/assignment.js
+++ src/models/assignment.js
@@ -4,17 +4,20 @@
 import mongoose from 'mongoose';
 import User from 'user.js';
 
+
+//Create model to store the following info for each assignment: courseID(from canvas), assignmentID(from canvas), studentID (from users), notes(input from user), code(input from user).
 const assignmentSchema = mongoose.Schema({
-  courseId:{type:String},
+  courseId:{type:String, required:true},
   assignmentId:{type:Number},
   code:{type:String},
   notes:{type:String},
   user: { type:mongoose.Schema.Types.ObjectId, ref:'user' },
 });
 
-assignmentSchema.pre('findOne', function(next) {
-  this.populate('user');
+assignmentSchema.pre('save', function(next) {
+ this.populate('user');
   next();
 });
+
 
 export default mongoose.model('assignment', assignmentSchema);
