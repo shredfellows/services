077a10604ca9a9d7a8e639081f429b42a3e8aa5b
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-13 17:36:20 -0700

    - finished the Find() method

--- src/models/github.js
+++ src/models/github.js
@@ -1,50 +1,71 @@
 'use strict';
 
-import mongoose from 'mongoose';
 import superagent from 'superagent';
 
-const githubSchema = mongoose.Schema({
-  id: {type: Number, required: true},
-  node_id: {type: String, required: true},
-  repo_name: {type: String, required: true},
-  language: {type: String},
-  private: {type: Boolean, required: true},
-  html_url: {type: String, required: true},
-  description: {type: String},
-  createdOn: {type: String, required: true},
-  updatedOn: {type: String, required: true}
-});
+const github = {};
+const baseURL = 'https://api.github.com/repos/shredfellows/assignments/contents/';
+const localHostURL = 'http://localhost:3000/api/v1/github/';
 
 
-githubSchema.pre('find', function(next) {
-  superagent.get('https://api.github.com/users/oviparasca/repos')
-  .set('Authorization', 'Bearer ' + '734ed377b3b0e52b9197d8a90fc6458030ee9095') // temporary
-  .then(res => {
-    console.log('hit the github repo url');
-    res => sendJSON(res, res.body);
-    // console.log(JSON.stringify(res.body));
+github.find = () => {
+  return superagent.get(baseURL)
+  .set({'Authorization': 'Bearer ' + '21c8bc3330e6393814e4b68321c8b5a9b6566d1a'})
+  .then(res => {   
+    return parseContents(res.body, baseURL);
   });
+};
 
-  next();
-});
-
-
-githubSchema.pre('findOne', function(next) {
-  next();
-
-  // https://api.github.com/repos/OviParasca/branches/00-ES6/master
-});
-
-
-
-let sendJSON = (res, data) => {
-  res.statusCode = 200;
-  res.statusMessage = 'OK';
-  res.setHeader('Content-Type', 'application/json');
-  res.write( JSON.stringify(data) );
-  res.end();
- };
+// TODO:
+github.findOne = (req) => {
+  console.log(baseURL + req._id.split(/[ .]+/).join('/'));
+  return superagent.get(baseURL + req._id.split(/[ .]+/).join('/'))
+  .set({'Authorization': 'Bearer ' + '21c8bc3330e6393814e4b68321c8b5a9b6566d1a'})
+  .then(res => {    
+    return res.body;
+  });
+};
 
 
 
-export default mongoose.model('github', githubSchema);
+// Helper functions
+
+function parseContents(data, url) {
+  const promises = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type === 'dir') {
+      promises.push(getSubDirectories(data[i].name, url+data[i].path));
+    }
+  }
+
+  return Promise.all(promises).then(values => {
+    var contents = {};
+    values.forEach(value => {
+      Object.keys(value).map(function (key){ 
+        console.log(value[key]);
+        console.log(key);
+        contents[key] = value[key];
+      });
+    });
+
+    return contents;
+  });
+};
+
+function getSubDirectories(key, directory) {
+  return superagent.get(directory)
+  .set({'Authorization': 'Bearer ' + '21c8bc3330e6393814e4b68321c8b5a9b6566d1a'})
+  .then(res => {  
+    var obj = {};
+    for (var i = 0; i < res.body.length; i++) {
+      if (res.body[i].type === 'dir') {
+        obj[res.body[i].name] = `${localHostURL}${res.body[i].path.replace('/', '.')}`;
+      }
+    }
+    var parentObj = {[key] : obj};
+    return parentObj;
+  });
+};
+
+
+export default github;
