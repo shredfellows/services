60ef32fcc2b2a977aaaa12bce7d9a764fb3fb065
Ben Hurst <BHurst8@gmail.com>
2018-06-14 10:50:46 -0700

    refactored github data object

--- src/models/github.js
+++ src/models/github.js
@@ -9,21 +9,25 @@ const localHostURL = 'http://localhost:3000/api/v1/github/';
 
 github.find = () => {
   return superagent.get(baseURL)
-    .set({'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN})
-    .then(res => {   
+    .set({
+      'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN
+    })
+    .then(res => {
       return parseContents(res.body, baseURL);
     });
 };
 
 // TODO:
 github.findOne = (req) => {
-  
+
   console.log(baseURL + req.split(/[ .]+/).join('/'));
   return superagent.get(baseURL + req.split(/[ .]+/).join('/'))
-    .set({'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN})
-    .then(res => {    
-    
-      return res.body;
+    .set({
+      'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN
+    })
+    .then(res => {
+
+      return parseFolder(res.body, baseURL + req.split(/[.]+/).join('/'));
     }).catch(console.error);
 };
 
@@ -36,14 +40,14 @@ function parseContents(data, url) {
 
   for (var i = 0; i < data.length; i++) {
     if (data[i].type === 'dir') {
-      promises.push(getSubDirectories(data[i].name, url+data[i].path));
+      promises.push(getSubDirectories(data[i].name, url + data[i].path));
     }
   }
 
   return Promise.all(promises).then(values => {
     var contents = {};
     values.forEach(value => {
-      Object.keys(value).map(function (key){ 
+      Object.keys(value).map(function (key) {
         console.log(value[key]);
         console.log(key);
         contents[key] = value[key];
@@ -56,18 +60,46 @@ function parseContents(data, url) {
 
 function getSubDirectories(key, directory) {
   return superagent.get(directory)
-    .set({'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN})
-    .then(res => {  
+    .set({
+      'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN
+    })
+    .then(res => {
       var obj = {};
       for (var i = 0; i < res.body.length; i++) {
         if (res.body[i].type === 'dir') {
           obj[res.body[i].name] = `${localHostURL}${res.body[i].path.replace('/', '.')}`;
         }
       }
-      var parentObj = {[key] : obj};
+      var parentObj = {
+        [key]: obj
+      };
       return parentObj;
     });
 }
 
+function parseFolder(data, url) {
+  var contents = {};
+  // const promises = [];
+
+  contents['name'] = url.split('/').pop().trim();
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name === 'challenges') {
+      contents[data[i].name] = ['https://github.com/shredfellows/assignments/blob/master/array/map/challenges/challenge1.md',
+        'https://github.com/shredfellows/assignments/blob/master/array/map/challenges/challenge2.md',
+      ];
+      // TODO: get the nested challenges
+    }
+    if (data[i].name === 'README.md') {
+      contents['readme'] = data[i].download_url;
+    }
+    // if (data[i].name === 'video') {
+    contents['video'] = 'https://www.youtube.com/embed/7nzYATGidVM';
+    //contents['video'] = data[i].url;
+    // }
+  }
+
+  return contents;
+}
 
 export default github;
