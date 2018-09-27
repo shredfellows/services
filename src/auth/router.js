489de5f1c089bdd4e7d2cd51e8c8bea7d2766db9
Justin Morris <justinjodymorris@gmail.com>
2018-09-26 23:58:28 -0700

    Revert "changed redirect to send client to /dashboard route rather than home"

--- src/auth/router.js
+++ src/auth/router.js
@@ -20,12 +20,11 @@ authRouter.get('/login', auth, (req, res, next) => {
 });
 
 authRouter.get('/oauth', (req, res, next) => {
-  let clientUrl = process.env.CLIENT_URL + '/dashboard';
   oauth.authorize(req)
     .then(token => {
-      res.cookie('Token', token, { domain: '.shredfellows.ccs.net' });
-      res.cookie('GHT', process.env.GITHUB_TOKEN, { domain: '.shredfellows.ccs.net' });
-      res.redirect(clientUrl);
+      res.cookie('Token', token, {domain: '.shredfellows.ccs.net'});
+      res.cookie('GHT', process.env.GITHUB_TOKEN, {domain: '.shredfellows.ccs.net'});
+      res.redirect(process.env.CLIENT_URL);
     })
     .catch(next);
 });
